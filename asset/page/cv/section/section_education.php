79b44b266c791a2653a1032eb0f6a45e4ab72821
tetronitte <68910823+tetronitte@users.noreply.github.com>
2021-01-27 21:13:41 +0100

    petit ajustement

--- asset/page/cv/section/section_education.php
+++ asset/page/cv/section/section_education.php
@@ -4,9 +4,9 @@
             <div class="col-3 date-education">
                 <p>2014 - 2019</p>
             </div>
-            <div class="row col-9 degree-school-education">
+            <div class="col-9 degree-school-education">
                 <span>Licence informatiques -</span>
-                <a href="https://www.u-picardie.fr/ufr/sciences/accueil/ufr-des-sciences-105111.kjsp" target="_blank" class="school-education" title="UPJV - UFR des sciences - Amiens">Université Picardie Jules Vernes - 80000, Amiens</a>
+                <a href="https://www.u-picardie.fr/ufr/sciences/accueil/ufr-des-sciences-105111.kjsp" target="_blank" class="school-education" title="UPJV - UFR des sciences - Amiens">Université Picardie Jules Vernes - 80000, Amiens</a><br/>
                 <p>Spécialisation informatique :<br/>
                     web,shell script, Base de données relationnelle, JAVA, C, langage assembleur</p>
             </div>
@@ -17,9 +17,9 @@
                     <div class="col-3 date-education">
                         <p>2013</p>
                     </div>
-                    <div class="row col-9 degree-school-education">
+                    <div class="col-9 degree-school-education">
                         <span>BAC S -</span>
-                        <a href="https://www.letudiant.fr/etudes/annuaire-des-lycees/lycee-general-et-technologique-lycee-des-metiers-marie-curie-6456.html" target="_blank" class="school-education" title="Lycée Marie Curie - Nogent sur Oise">Lycée Marie Curie - 60180, nogent sur Oise</a>
+                        <a href="https://www.letudiant.fr/etudes/annuaire-des-lycees/lycee-general-et-technologique-lycee-des-metiers-marie-curie-6456.html" target="_blank" class="school-education" title="Lycée Marie Curie - Nogent sur Oise">Lycée Marie Curie - 60180, nogent sur Oise</a><br/>
                         <p>Spécialité SVT</p>
                     </div>
                 </div>
